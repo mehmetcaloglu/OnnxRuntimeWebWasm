2acad40f08061367075adec84572523eb279584c
Mehmet Caloglu <mehmet.caloglu@arcelik.com>
2024-07-10 15:07:17 +0300

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,35 @@
 // next.config.mjs
-import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
-import CopyPlugin from 'copy-webpack-plugin';
+import NodePolyfillPlugin from "node-polyfill-webpack-plugin";
+import CopyPlugin from "copy-webpack-plugin";
 
 const nextConfig = {
-    reactStrictMode: true,
-    webpack: (config) => {
-        config.resolve.extensions.push('.ts', '.tsx');
-        config.resolve.fallback = { fs: false };
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.extensions.push(".ts", ".tsx");
+    config.resolve.fallback = { fs: false };
 
-        config.plugins.push(
-            new NodePolyfillPlugin(),
-            new CopyPlugin({
-                patterns: [
-                    {
-                        from: 'node_modules/onnxruntime-web/dist/ort-wasm.wasm',
-                        to: 'static/chunks/',
-                    },
-                    {
-                        from: 'node_modules/onnxruntime-web/dist/ort-wasm-simd.wasm',
-                        to: 'static/chunks/',
-                    },
-                    {
-                        from: 'model',
-                        to: 'static/chunks/pages/model',
-                    },
-                ],
-            })
-        );
+    config.plugins.push(
+      new NodePolyfillPlugin(),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: "node_modules/onnxruntime-web/dist/ort-wasm.wasm",
+            to: "static/chunks/",
+          },
+          {
+            from: "node_modules/onnxruntime-web/dist/ort-wasm-simd.wasm",
+            to: "static/chunks/app",
+          },
+          {
+            from: "model",
+            to: "static/chunks/pages/model",
+          },
+        ],
+      })
+    );
 
-        return config;
-    },
+    return config;
+  },
 };
 
 export default nextConfig;
